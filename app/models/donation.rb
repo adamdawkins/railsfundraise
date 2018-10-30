a80949b4865723604669f5e7f3c27af34f7bda36
Adam Dawkins <adam@dragondrop.uk>
2018-10-30 15:09:06 +0000

    ✅  Update view of donations

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -3,4 +3,8 @@ class Donation < ApplicationRecord
 
   validates_presence_of :amount
   validates_presence_of :campaign_id
+
+  def initials
+    self.name.split.map(&:first).join.upcase
+  end
 end
--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Donation, type: :model do
 
   it { is_expected.to validate_presence_of(:amount) }
   it { is_expected.to validate_presence_of(:campaign_id) }
+
+  describe "initials" do
+    it "returns the initials of the name" do
+      subject.name = 'Jonathan Bowen'
+      expect(subject.initials).to eq 'JB'
+    end
+  end
 end
