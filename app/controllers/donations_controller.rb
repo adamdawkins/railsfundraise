efb7358af2c226a71711744d3af826a57441f150
Adam Dawkins <adam@dragondrop.uk>
2018-10-31 20:30:08 +0000

    ✅ Accept donations from Donorbox post

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,12 +1,17 @@
 class DonationsController < ApplicationController
-  def create
-    @campaign = Campaign.friendly.find(params[:campaign_id])
-    @campaign.donations.create(donation_params)
+  before_action :set_campaign, only: [:create]
+  skip_before_action :verify_authenticity_token, only: [:create]
 
+  def create
+    donation = @campaign.donations.create(donation_params)
     redirect_to @campaign
   end
 
   private
+    def set_campaign
+      @campaign = Campaign.friendly.find(params[:campaign_id])
+    end
+
     def donation_params
       params.require(:donation).permit(:amount, :name, :is_anonymous, :comment)
     end
