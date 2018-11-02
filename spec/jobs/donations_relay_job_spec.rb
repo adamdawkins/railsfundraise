1ce0f1942a4d9822fe47afce734ec10c0d520c40
Adam Dawkins <adam@dragondrop.uk>
2018-11-02 22:10:08 +0000

    🚦 Fix DonationsRelayJob spec

--- spec/jobs/donations_relay_job_spec.rb
+++ spec/jobs/donations_relay_job_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe DonationsRelayJob, type: :job do
   describe "#perform" do
-    let(:donation) { FactoryBot.create(:donation) }
+    let(:donation) { double("donation") }
 
     before do
       allow(DonationsChannel).to receive(:broadcast_to)
       allow(DonationsController).to receive(:render).and_return(:some_html)
+      allow(donation).to receive(:campaign).and_return(:campaign)
     end
 
     it "broadcasts to DonationsChannel with the campaign html" do
