51b95f888cf800beae568d68f573dc5aea41b683
Adam Dawkins <adam@dragondrop.uk>
2018-11-04 20:11:19 +0000

    🚦 Update DonationsRelayJob spec to include raised amount

--- spec/jobs/donations_relay_job_spec.rb
+++ spec/jobs/donations_relay_job_spec.rb
@@ -3,17 +3,18 @@ require 'rails_helper'
 RSpec.describe DonationsRelayJob, type: :job do
   describe "#perform" do
     let(:donation) { double("donation") }
+    let(:campaign) { double("campaign", raised: 10) }
 
     before do
       allow(DonationsChannel).to receive(:broadcast_to)
       allow(DonationsController).to receive(:render).and_return(:some_html)
-      allow(donation).to receive(:campaign).and_return(:campaign)
+      allow(donation).to receive(:campaign).and_return(campaign)
     end
 
     it "broadcasts to DonationsChannel with the campaign html" do
       subject.perform(donation)
       expect(DonationsChannel).to(
-        have_received(:broadcast_to).with(donation.campaign, donation: :some_html)
+        have_received(:broadcast_to).with(donation.campaign, donation: :some_html, raised: 10)
       )
     end
   end
