874ae0481f738f10ad0acf78628784a56fe7028c
Adam Dawkins <adam@dragondrop.uk>
2018-11-05 22:57:09 +0000

    🚦 Add missing test for GET run-for-freedom

--- spec/controllers/landings_controller_spec.rb
+++ spec/controllers/landings_controller_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe LandingsController, type: :controller do
+  describe "GET /run-for-freedom" do
+    it "renders the run_for_freedom template" do
+      get :run_for_freedom
+
+      assert_template "landings/run_for_freedom"
+    end
+  end
+
   describe "GET /teachers" do
     it "renders the teachers template" do
       get :teachers
